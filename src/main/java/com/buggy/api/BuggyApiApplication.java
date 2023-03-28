a964ebcc7efc9a32db12561461753336f80a059d
Bradley Mwangangi <39657175+bradl33@users.noreply.github.com>
2023-03-28 06:46:34 +0300

    rename package from com.buggy to com.buggy.api

--- src/main/java/com/buggy/api/BuggyApiApplication.java
+++ src/main/java/com/buggy/api/BuggyApiApplication.java
@@ -1,4 +1,4 @@
-package com.buggy;
+package com.buggy.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/buggy/api/BuggyApiApplicationTests.java
+++ src/test/java/com/buggy/api/BuggyApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.buggy;
+package com.buggy.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
