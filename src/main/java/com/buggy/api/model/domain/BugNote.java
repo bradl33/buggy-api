f124082d645326958b17642d1c9ed55890af6513
Bradley Mwangangi <39657175+bradl33@users.noreply.github.com>
2023-03-31 23:51:56 +0300

    create models
    create enums - Role, ProjectStatus, BugStatus
    
    create domains - User, Project, Bug, BugNote

---
+++ src/main/java/com/buggy/api/model/domain/BugNote.java
@@ -0,0 +1,44 @@
+package com.buggy.api.model.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "bug_note_")
+public class BugNote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "bug_id", nullable = false)
+    private Bug bug;
+
+    @Column(name = "note_content", nullable = false)
+    private String noteContent;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_on", updatable = false, nullable = false)
+    private Date createdOn;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_on", nullable = false)
+    private Date updatedOn;
+}
---
+++ src/main/java/com/buggy/api/model/enums/Role.java
@@ -0,0 +1,21 @@
+package com.buggy.api.model.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum Role {
+    PROJECT_MANAGER("PROJECT_MANAGER"),
+    QA("QA"),
+    DEVELOPER("DEVELOPER"),
+    CLIENT("CLIENT");
+
+    private final String value;
+
+    Role(String value) {
+        this.value = value;
+    }
+
+    public String getValue(){
+        return value;
+    }
+}
